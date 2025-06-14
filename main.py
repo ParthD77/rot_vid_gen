2a244ce8e08e411ade583f5fa395da7cf7171955
Parth Dhroovji <parth.dhroovji@gmail.com>
2025-06-14 16:04:37 -0400

    working finished  version, title card added

--- main.py
+++ main.py
@@ -6,52 +6,74 @@ from pathlib import Path
 import whisper
 import string
 from openai import OpenAI
-# todo: 
-#   add the starting reddit image with the tet overlay
 
-# dependencies
-# pip install moviepy
-# pip install srt
-# download whisper from OpenAI
-# download imagemagick from chrome for moviepy to work  *on install check ADD to PATH and Install leacy utilities(convert)
-# OpenApi key must be set as a env vairable run this in cmd: $env:OPENAI_API_KEY="your-api-key-here" OR
-# go into computers env variables and add it there
-
-# To use, in one file location behind this code put your video and a text file with your story. Ensure video and text file have the same name
-# and then run the script. 
 
 # some stupid shit needed for it to not throw instilation error
 mpy_conf.change_settings({"IMAGEMAGICK_BINARY": "C:\\Program Files\\ImageMagick-7.1.1-Q16-HDRI\\magick.exe"})  # ADJUST this path if needed
 
+# some downgrade needed for pillow antialias
+from PIL import Image
+# Fix for deprecated Image.ANTIALIAS
+if not hasattr(Image, 'ANTIALIAS'):
+    Image.ANTIALIAS = Image.Resampling.LANCZOS
 
-current_dir = Path(__file__).parent
+
+current_dir = Path().resolve()
 parent_dir = current_dir.parent
+filename = input("Folder name (no .extension): ")
 
-fileloc = str(parent_dir / input("File name (no .extension): "))
+fileloc = str(parent_dir / filename / filename)
 
 # read the story
 text_file_path = fileloc + ".txt"
 with open(text_file_path, "r", encoding="utf-8") as f:
-    text = f.read().strip()
+    title = f.readline()
+    body = ''.join(f.readlines()).strip()
 
-"""
+
 # TEXT TO SPEECH
+# just the title
 client = OpenAI()
 with client.audio.speech.with_streaming_response.create(
     model="gpt-4o-mini-tts",
     voice="ash",
-    input=text,
+    input=title,
     speed=1.2
 ) as response:
-    response.stream_to_file(fileloc+".mp3")
-"""
-"""
+    response.stream_to_file(fileloc+"_title.mp3")
+
+# just the body
+with client.audio.speech.with_streaming_response.create(
+    model="gpt-4o-mini-tts",
+    voice="ash",
+    input=body,
+    speed=1.2
+) as response:
+    response.stream_to_file(fileloc+"_body.mp3")
+
+
+
+# TAKE VIDEO AUDIO AND COMBINE THEM
+# WRITE THE GENERATED SRT FILE TO THE VIDEO
+# Load video and audio
+video = mp.VideoFileClip(fileloc+".mp4").without_audio()
+
+# combine title and body audios
+audio_title = mp.AudioFileClip(fileloc+"_title.mp3")
+audio_body = mp.AudioFileClip(fileloc+"_body.mp3")
+full_audio = mp.concatenate_audioclips([audio_title, audio_body])
+
+# set the video to have audio
+video = video.set_audio(full_audio)
+
+
 
 # TAKE THE MP3 AND CREATE THE SRT FILE (SUBTITLES)
 # make subtitles from audio
 model = whisper.load_model("turbo")
-result = model.transcribe(fileloc+".mp3", verbose=False, word_timestamps=True)
+result = model.transcribe(fileloc+"_body.mp3", verbose=False, word_timestamps=True)
 
+off_set = audio_title.duration
 words_per_segment = 2
 subtitles = []
 index = 1
@@ -74,8 +96,8 @@ for segment in result['segments']:
             continue
 
         # Start and end time for the subtitle chunk
-        start_time = timedelta(seconds=word_group[0]['start'])
-        end_time = timedelta(seconds=word_group[-1]['end'])
+        start_time = timedelta(seconds=word_group[0]['start'] + off_set)
+        end_time = timedelta(seconds=word_group[-1]['end'] + off_set)
 
         # Build content string without punctuation
         content = ""
@@ -101,14 +123,33 @@ for segment in result['segments']:
 with open(fileloc+".srt", "w", encoding="utf-8") as f:
     f.write(srt.compose(subtitles))
 
-"""
 
-# TAKE VIDEO AUDIO AND COMBINE THEM
-# WRITE THE GENERATED SRT FILE TO THE VIDEO
-# Load video and audio
-video = mp.VideoFileClip(fileloc+".mp4").without_audio()
-audio = mp.AudioFileClip(fileloc+".mp3")
-video = video.set_audio(audio)
+# generate background png and title text
+title_bg = mp.ImageClip(str(parent_dir/"title.png")).set_duration(audio_title.duration)
+title_bg = title_bg.resize(width=video.w-50)  # match video width
+title_bg = title_bg.set_position(("center", "center"))
+
+
+text_width = title_bg.w
+text_height = title_bg.h
+text_pos = ((video.w - title_bg.w) // 2 + 20, (video.h - title_bg.h) // 2 + 60)
+
+title_text = mp.TextClip(
+    title,
+    fontsize=50,
+    color="black",
+    font="Arial-Bold",
+    method="caption",
+    size=(text_width, text_height),
+    align="West"
+).set_duration(audio_title.duration).set_position(text_pos)
+
+title_text = title_text.set_duration(audio_title.duration)
+title_clip = mp.CompositeVideoClip(
+    [title_bg, title_text],
+    size=(video.w, video.h)  
+).set_duration(audio_title.duration)
+
 
 
 # Load and parse the .srt file
@@ -140,12 +181,12 @@ for sub in subtitles:
     
 
     sub_clips.append(txt_clip)
-
+sub_clips.insert(0, title_clip)
 
 # Combine video with subtitles and trim it to end after story finishes
 ending_buffer = 0.5
 final = mp.CompositeVideoClip([video] + sub_clips)
-final = final.set_duration(audio.duration + ending_buffer)
+final = final.set_duration(full_audio.duration + ending_buffer)
 
 # Export final video
 final.write_videofile(fileloc+"_finished.mp4", codec="libx264", audio_codec="aac")
